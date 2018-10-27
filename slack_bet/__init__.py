dc41506335d4f27578ae31e14f30d9aca6254acb
mfrancis95 <mikefrancis95@gmail.com>
2018-10-27 11:50:58 -0400

    React with :bet: whenever a user reacts with :bet: (resolve #1)

--- slack_bet/__init__.py
+++ slack_bet/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from re import IGNORECASE, match, search
 from os import environ
 from requests import post
@@ -9,15 +9,28 @@ app = Flask(__name__)
 def main():
     if request.json['type'] == 'url_verification':
         return request.json['challenge']
+    if request.json['event']['type'] == 'message':
+        _message()
+    elif request.json['event']['type'] == 'reaction_added':
+        _reaction()
+    return ''
+
+def _message():
     try:
         if match('bet$', request.json['event']['text'], IGNORECASE) or search(':bet:', request.json['event']['text'], IGNORECASE):
-            arguments = {
-                'channel': request.json['event']['channel'],
-                'name': 'bet',
-                'timestamp': request.json['event']['ts'],
-                'token': environ['OAUTH_TOKEN']
-            }
-            post('https://slack.com/api/reactions.add', arguments)
+            _react(request.json['event']['channel'], request.json['event']['ts'])
     except:
         pass
-    return ''
+
+def _react(channel, timestamp):
+    arguments = {
+        'channel': channel,
+        'name': 'bet',
+        'timestamp': timestamp,
+        'token': environ['OAUTH_TOKEN']
+    }
+    post('https://slack.com/api/reactions.add', arguments)
+
+def _reaction():
+    if request.json['event']['reaction'] == 'bet':
+        _react(request.json['event']['item']['channel'], request.json['event']['item']['ts'])
